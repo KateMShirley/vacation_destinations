7f3d3ca0fb43d61033d98af7edf16451bb1edcde
KateMShirley <kate.m.shirley@gmail.com>
2015-08-14 16:56:47 -0500

    sessions codealong with James

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,13 +2,28 @@ class SessionsController < ApplicationController
   #get
   def login
     #form to login
+
   end
 #post
   def confirmation
     #show user message regarding login status
+    puts"------------"
+    puts puts
+    puts"--------------"
+    @user = User.find_by_email(params[:email])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/'
+
+    else
+      @message = 'invalid'
+      render '/sessions/login'
   end
-#get 
+#get
   def logout
     #logout
+    session[:user_id] = nil
+    reidirect_to '/users/login'
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,38 @@ class UsersController < ApplicationController
   #get
   def signup
     #render a view and let the user register
+    #my only job...is to render a form! yay i have a job
   end
-  #POST 
+  #POST
   def confirmation
+    #debugging
+    # puts "---------users#confirmation---------"
+    # puts user_params
+    # puts "---------end users#confirmation---------"
     #post request that handles registration
+
+    @user = User.new(user_params)
+    if @user.save
+      #thanks! you're in!
+      session[:user_id] = @user.id
+
+      # Destination.where(:user_id => session[:user_id].to_i)
+    else
+      @message= 'User account exists or passwords do not match'
+      render 'users/signup'
+      #womp womp error msg
+      @message = 'User account exitst or passwords do not match.'
+      render 'users/singup'
+    end
+
   end
+
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+
 end
